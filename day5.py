7f76a91a882da6ab6927ea1061d311849b0ed8e8
mofr <mofr@zond.org>
2016-12-05 23:34:50 +0700

    Simplify a bit password initialization

--- day5.py
+++ day5.py
@@ -8,14 +8,14 @@ common_hash = hashlib.md5()
 common_hash.update(input.encode())
 start = time.clock()
 i = 0
-password = ['_', '_', '_', '_', '_', '_', '_', '_']
+password = ['_'] * 8
 while True:
     hash = common_hash.copy()
     hash.update(str(i).encode())
     md5 = hash.hexdigest()
     if md5.startswith('00000'):
         pos = int(md5[5], 16)
-        if pos < 8 and password[pos] == '_':
+        if pos < len(password) and password[pos] == '_':
             password[pos] = md5[6]
             print(''.join(password))
             if '_' not in password:
