38eeca0239908cf46d648b5284a0a77b65142a14
mofr <mofr@zond.org>
2016-12-06 23:12:27 +0700

    Add day6

---
+++ day6.py
@@ -0,0 +1,20 @@
+def most_common(lst):
+    return max(set(lst), key=lst.count)
+
+
+def least_common(lst):
+    return min(set(lst), key=lst.count)
+
+with open('day6_input') as f:
+    input = f.read()
+
+columns = []
+for _ in range( len(input.split()[0])):
+    columns.append([])
+
+for line in input.split():
+    for i, c in enumerate(line):
+        columns[i].append(c)
+
+print(''.join(map(least_common, columns)))
+print(''.join(map(most_common, columns)))
