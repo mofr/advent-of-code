1dd67fb73f32fbc4f832461a7bf0b0a965c08772
mofr <mofr@zond.org>
2016-12-11 20:06:45 +0700

    Add one more case for day 11

--- day11.py
+++ day11.py
@@ -3,10 +3,11 @@ def elevate(input):
     item_count = 0
     for line in input.split('\n'):
         if item_count > 0:
-            step_count += (item_count - 2) * 2 + 1
+            step_count += 2*item_count - 3
         item_count += line.count('generator')
         item_count += line.count('chip')
     return step_count
 
 print(elevate(open('day11_input1').read()))
 print(elevate(open('day11_input2').read()))
+print(elevate(open('day11_input3').read()))
