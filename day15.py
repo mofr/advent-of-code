5003b6039b3de81a2f8e3a13d07622295d63b7cd
mofr <mofr@zond.org>
2016-12-20 23:23:54 +0700

    Add day 15

---
+++ day15.py
@@ -0,0 +1,28 @@
+with open('day15_input') as f:
+    input = f.read()
+
+
+class Disc:
+    def __init__(self, pos, count):
+        self.pos = pos
+        self.count = count
+
+discs = []
+
+for i, line in enumerate(input.split('\n')):
+    splitted = line.split()
+    count = int(splitted[3])
+    pos = (int(splitted[-1][:-1]) + i + 1) % count
+    discs.append(Disc(pos, count))
+
+time = 0
+while True:
+    for disc in discs:
+        if (disc.pos + time) % disc.count != 0:
+            break
+    else:
+        break
+
+    time += 1
+
+print(time)
